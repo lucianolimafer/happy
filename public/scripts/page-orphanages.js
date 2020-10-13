cf6765ee4fb5b7c86e8838a82443fd35283f8717
lucianolimafer <lucianolimaf3r@gmail.com>
2020-10-13 13:34:38 -0300

    Add popup leaflet

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,12 +1,29 @@
+
+// create map
 const happyMap = L.map('mapid').setView([-30.0826464, -51.2131719], 14);
 
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-    {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(happyMap);
 
-L.marker([-30.0826464, -51.2131719])
+// create and add tilelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(happyMap);
+
+
+// create icon
+const icon = L.icon({
+    iconUrl: "../public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Cantinho da Vó Georgina <a href="orphanages.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+// create marker
+L.marker([-30.0826464, -51.2131719], { icon })
     .addTo(happyMap)
-    .bindPopup('cantinho Vó Georgina')
-    .openPopup();
+    .bindPopup(popup)
