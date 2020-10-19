0b0c58a7f5e5b0ac7ba36eec5570ef609a2fd3a2
lucianolimafer <lucianolimaf3r@gmail.com>
2020-10-19 20:46:26 -0300

    Fake data

---
+++ src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-30.0826464",
+        lng: "-51.2131719",
+        name: "lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1586022773518-47a6bf08fa90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1596962248954-a0381dc10a20?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha se sentir a vontate e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h às 18h",
+        opening_on_weekends: "1"
+
+    },
+
+    {
+        id: 2,
+        lat: "-30.0826464",
+        lng: "-51.2231719",
+        name: "lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1586022773518-47a6bf08fa90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1596962248954-a0381dc10a20?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha se sentir a vontate e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h às 18h",
+        opening_on_weekends: "0"
+
+    }
+]
