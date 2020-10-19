f0a92a47a7fcff1774ff99c5d8f95712e27af7cc
lucianolimafer <lucianolimaf3r@gmail.com>
2020-10-19 20:47:08 -0300

    Change path

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -9,7 +9,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(happyMap
 
 // create icon
 const icon = L.icon({
-    iconUrl: "../public/images/map-marker.svg",
+    iconUrl: "./images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
@@ -21,7 +21,7 @@ const popup = L.popup({
     className: "map-popup",
     minWidth: 240,
     minHeight: 240
-}).setContent('Cantinho da Vó Georgina <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+}).setContent('Cantinho da Vó Georgina <a href="orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>')
 
 // create marker
 L.marker([-30.0826464, -51.2131719], { icon })
