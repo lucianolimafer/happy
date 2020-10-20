773e1ecb3e55e4dbefee2c01111536f02591641f
lucianolimafer <lucianolimaf3r@gmail.com>
2020-10-20 10:46:04 -0300

    Fix bug

--- src/database/test.js
+++ src/database/test.js
@@ -7,6 +7,7 @@ Database.then(async db => {
             lat,
             lng,
             name,
+            about,
             whatsapp,
             images,
             instructions,
@@ -16,6 +17,7 @@ Database.then(async db => {
             "-30.0826464",
             "-51.2131719",
             "lar das meninas",
+            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
             "1654651232",
             "https://images.unsplash.com/photo-1586022773518-47a6bf08fa90?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
             "Venha se sentir a vontate e traga muito amor e paciência para dar.",
@@ -25,4 +27,7 @@ Database.then(async db => {
     `)
 
     // see data
+    const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+
+    console.log(selectedOrphanages)
 })
