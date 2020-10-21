685859beae92311aac832444718fa29aa25b0cc2
lucianolimafer <lucianolimaf3r@gmail.com>
2020-10-21 00:21:19 -0300

    Add functions async

--- src/pages.js
+++ src/pages.js
@@ -1,4 +1,5 @@
-const orphanages = require('./database/fakedata.js');
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage');
 
 module.exports = {
 
@@ -8,14 +9,35 @@ module.exports = {
 
     },
 
-    orphanage(req, res){
-        return res.render('orphanage')
+    async orphanage(req, res){
+        const id = req.query.id;
+
+        try {
+            const db = await Database;
+            const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+            const orphanage = results[0];
+
+            orphanage.images = orphanage.images.split(",")
+            
+            return res.render('orphanage', { orphanage: orphanage[0]})
+
+        } catch (error) {
+            console.log(error);
+            return res.send('Erro no BD')
+        }
 
     },
 
-    orphanages(req, res) {
-        return res.render('orphanages', { orphanages })
-
+    async orphanages(req, res) {
+        // put with bd
+        try{
+            const db = await Database;
+            const orphanages = await db.all("SELECT * FROM orphanages")
+            return res.render('orphanages', { orphanages })
+        } catch (error){
+            console.log(error)
+            return res.send('Erro no BD')
+        } 
     },
 
     createOrphanages(req, res) {
